f26ee051b83d7485a9ff2fdcbd899a77083d219b
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-03-07 12:00:36 +0100

    initial commit

---
+++ xenial-git.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+mkdir -p /etc/dpkg/dpkg.cfg.d
+cat >/etc/dpkg/dpkg.cfg.d/01_nodoc <<EOF
+path-exclude /usr/share/doc/*
+path-include /usr/share/doc/*/copyright
+path-exclude /usr/share/man/*
+path-exclude /usr/share/groff/*
+path-exclude /usr/share/info/*
+path-exclude /usr/share/lintian/*
+path-exclude /usr/share/linda/*
+EOF
+
+export DEBIAN_FRONTEND=noninteractive
+export APTARGS="-qq -o=Dpkg::Use-Pty=0"
+apt-get update ${APTARGS}
+apt-get install ${APTARGS} -y git
+apt-get clean
